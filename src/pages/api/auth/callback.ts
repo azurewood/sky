11621d2f3113a69d343edf7dc919c32a1fd1e14c
Jian <wood.azure@gmail.com>
2024-01-26 14:08:08 +0800

    auth remove debug

--- src/pages/api/auth/callback.ts
+++ src/pages/api/auth/callback.ts
@@ -4,7 +4,7 @@ import { supabase } from "../../../lib/supabase";
 export const GET: APIRoute = async ({ url, cookies, redirect }) => {
   const authCode = url.searchParams.get("code");
 
-  console.log(authCode)
+  // console.log(authCode)
   if (!authCode) {
     return new Response("No code provided", { status: 400 });
   }
@@ -16,7 +16,7 @@ export const GET: APIRoute = async ({ url, cookies, redirect }) => {
   }
 
   const { access_token, refresh_token } = data.session;
-  console.log(access_token, refresh_token)
+  // console.log(access_token, refresh_token)
 
   cookies.set("sb-access-token", access_token, {
     path: "/",
