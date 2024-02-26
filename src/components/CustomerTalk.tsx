58c80a525b84cb5918d2f1a409f3b4d9e2627d89
Jian <wood.azure@gmail.com>
2024-02-26 23:12:25 +0800

    clear textarea

--- src/components/CustomerTalk.tsx
+++ src/components/CustomerTalk.tsx
@@ -1,45 +1,51 @@
 import { createSignal, type JSX } from "solid-js";
 
 
-const CustomerTalk = ({ from, token }: { from: string, token:string }) => {
+const CustomerTalk = ({ from, token }: { from: string, token: string }) => {
   const [open, setOpen] = createSignal(false);
-const [response, setResponse] = createSignal("");
+  const [response, setResponse] = createSignal("");
+  const [content, setContent] = createSignal("");
 
-const handleClose = (_: any) => {
-  setOpen(false);
-  // console.log(open(),"xxx");
-}
-
-const handleOpen = (_: any) => {
-  setOpen(true);
-  // console.log(open(),"yyy");
-}
-
-// async function submit(e: SubmitEvent) {
-const onSubmit: JSX.EventHandler<HTMLFormElement, SubmitEvent> = async (
-  e,
-) => {
-  e.preventDefault();
-  setResponse("");
-  const formElement = e.currentTarget;
-  const formData = new FormData(formElement);
-  // const formData = new FormData(e.target as HTMLFormElement);
-  const content=formData.get("content");
-  if(content===undefined||content?.toString()===undefined||content?.toString().length<5)
-  {
-    setResponse("Message is too short!");
-    return;
+  const handleClose = (_: any) => {
+    setOpen(false);
+    // console.log(open(),"xxx");
   }
-  const response = await fetch("/api/message.json", {
-    method: "POST",
-    body: JSON.stringify({ from, content, token}), //formData,
-  });
-  // console.log(response)
-  const data = await response.json();
-  if (data.error) {
-    setResponse(data.error);
+
+  const handleOpen = (_: any) => {
+    setOpen(true);
+    // console.log(open(),"yyy");
   }
-}
+
+  // async function submit(e: SubmitEvent) {
+  const onSubmit: JSX.EventHandler<HTMLFormElement, SubmitEvent> = async (
+    e,
+  ) => {
+    e.preventDefault();
+    setResponse("");
+    const formElement = e.currentTarget;
+    const formData = new FormData(formElement);
+    // const formData = new FormData(e.target as HTMLFormElement);
+    const content = formData.get("content");
+    if (content === undefined || content?.toString() === undefined || content?.toString().length < 5) {
+      setResponse("Message is too short!");
+      return;
+    }
+    const response = await fetch("/api/message.json", {
+      method: "POST",
+      body: JSON.stringify({ from, content, token }), //formData,
+    });
+    // console.log(response)
+    const data = await response.json();
+    if (data.error) {
+      setResponse(data.error);
+    }
+    setContent("");
+  }
+
+  const keyUpHandler: JSX.EventHandlerUnion<HTMLTextAreaElement, KeyboardEvent> = (e,) => {
+    // console.log(e.currentTarget.value);
+    setContent(e.currentTarget.value);
+  }
 
   return (
     <div class="flex justify-between text-blue-200 shadow-inner rounded px-0 py-3 bg-blue-600">
@@ -49,7 +55,7 @@ const onSubmit: JSX.EventHandler<HTMLFormElement, SubmitEvent> = async (
         {open() ? <form onSubmit={onSubmit}>
           <div class="w-96 flex flex-col">
             <div class="relative w-full min-w-[200px]">
-              <textarea placeholder="" name="content"
+              <textarea placeholder="" name="content" onKeyUp={keyUpHandler} value={content()}
                 class="peer h-full min-h-[100px] w-full resize-none border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-900 focus:outline-0 disabled:resize-none disabled:border-0 disabled:bg-blue-gray-50"></textarea>
               <label
                 class="after:content[' '] pointer-events-none absolute left-0 -top-2.5 flex h-full w-full select-none text-sm font-normal leading-tight text-blue-gray-500 transition-all after:absolute after:-bottom-1 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-900 after:transition-transform after:duration-300 peer-placeholder-shown:leading-tight peer-placeholder-shown:text-blue-gray-500 peer-focus:text-sm peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
