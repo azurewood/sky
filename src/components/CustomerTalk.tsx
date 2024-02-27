88381ea0287c4024262b02eb8a86fbdf73222299
Jian <wood.azure@gmail.com>
2024-02-27 12:15:32 +0800

    button design

--- src/components/CustomerTalk.tsx
+++ src/components/CustomerTalk.tsx
@@ -65,7 +65,7 @@ const CustomerTalk = ({ from }: { from: string }) => {
                 name="from"
                 value={from}></input> */}
             </div>
-            <button class="w-32 self-center border shadow active:translate-y-px active:translate-x-px"><strong>OK</strong></button>
+            <button class="w-32 mb-2 self-center select-none border shadow active:translate-y-px active:translate-x-px dark:bg-zinc-100 bg-zinc-900 border-zinc-900 py-1.5 dark:border-zinc-100 rounded-md mt-2 dark:text-zinc-900 text-zinc-100 font-medium text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 dark:focus:ring-offset-zinc-700 disabled:opacity-50 disabled:cursor-not-allowed">OK</button>
             {response() && <p class="text-red-700">{response()}</p>}
           </div>
         </form> : <></>}
@@ -74,12 +74,12 @@ const CustomerTalk = ({ from }: { from: string }) => {
 
       {open() ?
         <div>
-          <button class="px-3" onClick={handleClose}><strong class="text-2xl cursor-pointer">&times;</strong></button>
+          <button class="px-3" onClick={handleClose}><strong class="text-2xl cursor-pointer select-none">&times;</strong></button>
           <div></div>
         </div>
         :
         <div>
-          <button class="px-3" onClick={handleOpen}><strong class="text-2xl cursor-pointer">&boxbox;</strong></button>
+          <button class="px-3" onClick={handleOpen}><strong class="text-2xl cursor-pointer select-none">&boxbox;</strong></button>
           <div></div>
         </div>}
 
