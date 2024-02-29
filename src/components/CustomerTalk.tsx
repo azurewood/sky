0bee63d4d47056a0c7f23b13471307be2d68f199
Jian <wood.azure@gmail.com>
2024-02-29 23:00:55 +0800

    overflow clip & input handler

--- src/components/CustomerTalk.tsx
+++ src/components/CustomerTalk.tsx
@@ -47,7 +47,7 @@ const CustomerTalk = ({ from, owner, sending, setSending }: { from: string, owne
     setSending(false);
   }
 
-  const keyUpHandler: JSX.EventHandlerUnion<HTMLTextAreaElement, KeyboardEvent> = (e,) => {
+  const inputHandler: JSX.InputEventHandler<HTMLTextAreaElement, InputEvent> = (e,) => {
     // console.log(e.currentTarget.value);
     setContent(e.currentTarget.value);
   }
@@ -72,7 +72,7 @@ const CustomerTalk = ({ from, owner, sending, setSending }: { from: string, owne
         {open() ? <form onSubmit={onSubmit}>
           <div class="flex flex-col px-5 gap-y-1">
             <div class="relative w-full min-w-[200px]">
-              <textarea placeholder="" name="content" id="content" onKeyUp={keyUpHandler} value={content()}
+              <textarea placeholder="" name="content" id="content" onInput={inputHandler} value={content()}
                 class="peer h-full min-h-[100px] w-full resize-none border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 rounded-none outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-900 focus:outline-0 disabled:resize-none disabled:border-0 disabled:bg-blue-gray-50"></textarea>
               <label for="content"
                 class="after:content[' '] pointer-events-none absolute left-0 -top-2.5 flex h-full w-full select-none text-sm font-normal leading-tight text-blue-gray-500 transition-all after:absolute after:-bottom-1 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-900 after:transition-transform after:duration-300 peer-placeholder-shown:leading-tight peer-placeholder-shown:text-blue-gray-500 peer-focus:text-sm peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
